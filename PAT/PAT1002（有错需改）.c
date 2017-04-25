281e22a55c99c56606287f6d181a044d4942219e
fengzi2016 <2239056373@qq.com>
2017-04-25 20:14:52 +0800

    PAT1002

---
+++ PAT/PAT1002（有错需改）.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int n,x,p,t,j,f;
+	scanf("%d",&n);
+	x=0;
+	while(n!=0)
+	{
+		x=x+n%10;
+		n=n/10;
+	} 
+	j=0;
+	f=x;
+   while(f){
+   	j++;
+   	f/=10;
+   }
+ 	int a[j];	
+ 		for(t=j-1;t>=0;t--)
+    	{
+	    	a[t]=x%10;
+	    	x=x/10;
+	    }
+    for(t=0;t<j;t++)
+    {
+    	switch(a[t])
+    	{
+   		case 0:printf("ling ");break; 
+    	case 1:printf("yi ");break;
+    	case 2:printf("er ");break;
+    	case 3:printf("san ");break;
+    	case 4:printf("si ");break; 	
+    	case 5:printf("wu ");break;
+		case 6:printf("liu ");break;
+		case 7:printf("qi ");break;
+		case 8:printf("ba ");break;
+		case 9:printf("jiu ");break;	
+    	}
+    	
+    }if(t>=2) printf("\b");
+
+    return 0;
+}
