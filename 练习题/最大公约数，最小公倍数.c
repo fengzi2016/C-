7c41c9e2a44c9b09517a7bca575cfc889243e2f3
fengzi2016 <2239056373@qq.com>
2017-05-04 22:04:21 +0800

    模型1

---
+++ 练习题/最大公约数，最小公倍数.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+int main(){
+  	int  product,  gongyue, gongbei,   m,   n,   temp;
+                                      
+  	printf("请输入两个正整数m,  n:");
+  	scanf("%d%d",&m,&n); 
+    if (m<n) {temp=m;m=n;n=temp;} 
+	        		  product = m*n;     
+   	                  gongyue=m%n; 	    
+                      while(gongyue!=0){                             
+   					   m = n;
+    		           n = gongyue;
+   		               gongyue=m%n;
+                      }
+                   gongbei = product/n;                  //求最小公倍
+                   
+                   printf("它们的最大公约数为:%d\n",  n);
+                   printf("它们的最小公倍数为:%d\n",  gongbei);
+                   return 0;
+ }
