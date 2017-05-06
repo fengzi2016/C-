759b40b255fc4604e696e84c4418921aea7cd5ba
fengzi2016 <2239056373@qq.com>
2017-05-06 21:37:25 +0800

    PAT1010

---
+++ PAT/PAT1010(1).c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+#include<math.h>
+int main(){
+	int i,n,j;
+	scanf("%d",&n);
+    double a[n][3];
+	for(i=0;i<n;i++){
+		for(j=0;j<3;j++){
+			scanf("%lf",&a[i][j]);		
+		}
+		if(a[i][0]+a[i][1]>a[i][2])
+			printf("Case #%d: ture\n",i+1);
+			else printf("Case #%d: false\n",i+1);					
+	}
+	return 0;
+	
+} 
