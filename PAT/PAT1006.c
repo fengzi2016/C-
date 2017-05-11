37bac581107047b1e77ad04871b81cc1dd2edcab
fengzi2016 <2239056373@qq.com>
2017-05-11 16:31:08 +0800

    PAT1006

---
+++ PAT/PAT1006.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int main() {
+	int n,i,j,bai,shi,ge;
+	scanf("%d",&n);
+	ge=n%10;
+	shi=n/10%10;
+	bai=n/100;	
+	for(j=0;j<bai;j++)
+	printf("B");
+	for(j=0;j<shi;j++)
+	printf("S");
+	for(j=1;j<=ge;j++)
+	printf("%d",j);
+	return 0;
+}
