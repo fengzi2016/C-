41abeefbc72d9086b2b55f7689eda6da4ab4d882
fengzi2016 <2239056373@qq.com>
2017-05-20 19:35:54 +0800

    good

---
+++ PAT/PAT1009（有错，出界）.c
@@ -0,0 +1,28 @@
+﻿#include<stdio.h>
+#include<string.h>
+int main(){
+	char *point_1,*point_2;
+	char dayin(char *p,char *q);
+	char ch[80];
+	gets(ch);
+	point_1=point_2=&ch[strlen(ch)-1];
+	dayin(point_1,point_2);
+	return 0;
+}
+char dayin(char *p,char *q){
+	int temp;
+	while(p>=0){
+		while(*p!=' '&&q>=0){	
+			p--;
+		}
+		q=temp=p+1;
+		while(*q!=' '&&*q!='\0'){
+			printf("%c",*q);	
+			q++;		
+		}
+		printf(" ");
+		p=q=temp-2;
+		
+	}	
+	return 0;
+}
---
+++ PAT/PAT1036.c
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+int main(){
+	int n,i,j,m;
+	char ch;
+	scanf("%d ",&n);
+	scanf("%c",&ch);
+	if(n%2==0)m=n/2;
+	else m=(n+1)/2;
+	for(i=0;i<m;i++){
+		if(i==0||i==m-1){
+			for(j=0;j<n;j++)
+				printf("%c",ch);
+		}
+		else{
+			for(j=0;j<n;j++){
+				if(j==0||j==n-1){
+					printf("%c",ch);
+				}
+				else printf(" ");				
+			}
+			
+		}printf("\n");		
+			
+	}
+	return 0;
+	
+} 
