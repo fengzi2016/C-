394ffce833d54093ca94ce8022db0f1cd3914b05
fengzi2016 <1820166258@qq.com>
2017-06-02 22:51:38 +0800

    jiegouti
    结构体练习

---
+++ practice/jiegouti.c
@@ -0,0 +1,35 @@
+﻿#include<stdio.h>
+int main(){
+	struct Person{
+		int num;
+		char name[8];
+		float grade;
+	};
+	int i,j,k,n;
+	int sum;
+	float averge;
+ 	//struct Person *s = (struct Person*)malloc(s*sizeof(struct Person));
+	printf("请输入学生数量:\n");
+	scanf("%d",&n);
+	printf("请输入学生的学号，姓名，成绩:\n");
+	struct Person s[n],b[n];
+	for(i=0;i<n;i++){
+		scanf("%d%s%f",&s[i].num,s[i].name,&s[i].grade);
+	}
+	sum=0;
+	for(j=0;j<n;j++){
+		sum=s[j].grade+sum;
+	}
+	averge=sum/n;
+	printf("平均分为%2.1f\n",averge);
+	k=0;
+	for(i=0;i<n;i++){
+		if(s[i].grade<averge){
+			b[k++]=s[i];
+		}
+	}
+	
+	for(i=0;i<k;i++)
+		printf("%d,%s,%2.1f",b[i].num,b[i].name,b[i].grade);
+	return 0;
+}
---
+++ practice/jiegouti2.c
@@ -0,0 +1,46 @@
+﻿#include<stdio.h>
+struct Person
+		{ 	int num;
+			char name[8];
+		 	char sex;
+ 		 	int age;
+			float grade;
+		}stu[10]={
+		{101,"Zhang",'M',19,95.6},
+		{102,"Wang",'F',18,92.4},
+		{103,"Zhao",'M',19,85.7},
+		{104,"Li",'M',20,96.3},
+		{105,"Gao",'M',19,90.2},
+		{106,"Lin",'M',18,91.5},
+		{107,"Ma",'F',17,98.7},
+		{108,"Zhen",'M',19,89.8},
+		{109,"Xu",'M',19,89.8}, 
+		{110,"Mao",'F',18,94.9},
+		};	 	
+		int main(){
+			int i,j;
+			struct Person temp;
+			printf("学号 姓名 性别 年龄  成绩\n");
+			for(i=0;i<10;i++){
+			printf("%4d %5s %2c %5d  %2.1f\n",stu[i].num,stu[i].name,stu[i].sex,stu[i].age,stu[i].grade);
+			}
+			for(j=0;j<9;j++)
+				for(i=0;i<9-j;i++){
+					if(stu[i].grade>stu[i+1].grade){
+						temp=stu[i];
+						stu[i]=stu[i+1];
+						stu[i+1]=temp;
+					}
+				}
+			printf("学号 姓名 性别 年龄  成绩\n");
+			for(i=0;i<10;i++){
+			printf("%4d %5s %2c %5d  %2.1f\n",stu[i].num,stu[i].name,stu[i].sex,stu[i].age,stu[i].grade);
+			}
+			return 0;
+		}
+		/*put(){
+			printf("学号 姓名 性别 年龄  成绩\n");
+			for(i=0;i<10;i++){
+			printf("%4d %5s %2c %5d  %2.1f\n",stu[i].num,stu[i].name,stu[i].sex,stu[i].age,stu[i].grade);
+			}
+		*/
