fee39545e27582f25a992a9f9be553ee0ba2ef51
fengzi2016 <1820166258@qq.com>
2017-06-04 22:07:00 +0800

    动态分配内存

--- PAT/PAT1043（not end）.c
+++ PAT/PAT1043（not end）.c
@@ -1,11 +1,12 @@
 ﻿#include<stdio.h>
 #include<string.h>
 int main(){
-	char *point;int i,j;
+	int i,j;char *point;
 	int max(int i,int j);
-	void fuzhi(char *p,int n,char j);
+	//void fuzhi(char *p,int n,char j);
 	int count_P,count_A,count_T,count_e,count_s,count_t;
 	count_P=count_A=count_T=count_e=count_s=count_t=0;
+	int s[6];
 	char m[10000];
 	gets(m);
 	point=m;
@@ -21,26 +22,41 @@ int main(){
 		}
 		point++;
 	}
-	char P[count_P],A[count_A],T[count_T],e[count_e],s[count_s],t[count_t];
-	fuzhi(P,count_P,'P');	fuzhi(A,count_A,'A');	fuzhi(T,count_T,'T');	
-	fuzhi(e,count_e,'e');fuzhi(s,count_s,'s');	fuzhi(t,count_t,'t');
+	//char P[count_P],A[count_A],T[count_T],e[count_e],s[count_s],t[count_t];
+//	fuzhi(P,count_P,'P');	fuzhi(A,count_A,'A');	fuzhi(T,count_T,'T');	
+//	fuzhi(e,count_e,'e');fuzhi(s,count_s,'s');	fuzhi(t,count_t,'t');
 	int o,q,l;
+	s[6]=(count_P,count_A,count_T,count_e,count_s,count_t);
 	o=max(count_P,count_A);q=max(count_T,count_e);l=max(count_s,count_t);
 	o=max(o,q);q=max(l,q);l=max(o,q);
 	for(i=0;i<l;i++){
-		printf("%c%c%c%c%c%c",P[i],A[i],T[i],e[i],s[i],t[i]);
-		/*有错输出乱码*/
+		for(j=0;j<6;j++){
+			if(s[j]){
+				s[j]--;
+			}
+			switch (j){
+				case 0:printf("P");break;
+				case 1:printf("A");break;
+				case 2:printf("T");break;
+				case 3:printf("e");break;
+				case 4:printf("s");break;
+				case 5:printf("t");break;
+				default:break; 
+			}
+			
+		} 
 	}
+	printf("\n");	
 	return 0;
 	
 } 
-void fuzhi(char *p,int n,char j){
+/*void fuzhi(char *p,int n,char j){
 	int i;
 	for(i=0;i<n;i++){
 		*p=j;
 	}
 	
-}
+}*/
 int max(int i,int j){
 	if(i>j)return i;
 	else return j;
