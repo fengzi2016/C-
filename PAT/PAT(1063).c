ad3618bcc4919e5f313ae179a4e205b14941bc47
fengzi2016 <1820166258@qq.com>
2017-06-10 20:45:11 +0800

    PAT2题

---
+++ PAT/PAT(1063).c
@@ -0,0 +1,32 @@
+﻿#include<stdio.h>
+#include<math.h>
+int main(){
+	int n,i,j,x,y;
+	float temp;
+	scanf("%d",&n);
+	int a[n][2];
+	float b[n];
+	for(i=0;i<n;i++){
+		for(j=0;j<2;j++){
+			scanf("%d",&a[i][j]);
+		}			
+	}
+	for(i=0;i<n;i++){
+		for(j=0;j<2;j++){
+			x=a[i][0]*a[i][0];
+			y=a[i][1]*a[i][1];
+			b[i]= sqrt(x+y);
+		}
+	}
+	for(i=0;i<n-1;i++){
+		for(j=i+1;j<n;j++){
+			if(b[i]<b[j]){
+				temp=b[i];
+				b[i]=b[j];
+				b[j]=temp;
+			}
+		}
+	}
+	printf("%3.2f",b[0]);
+	return 0;
+} 
---
+++ PAT/PAT1064(有错需改).c
@@ -0,0 +1,52 @@
+﻿#include<stdio.h>
+int main(){
+	int j,temp,n,count;
+	int qiuhe(int x);
+	scanf("%d",&n);
+	int a[n];
+	int i;
+	int b[n],c[n];
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+		b[i]=qiuhe(a[i]);
+	}
+	for(i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+			if(b[i]>b[j]){
+				temp=b[i];
+				b[i]=b[j];
+				b[j]=temp;
+			}
+		}
+	}
+	count=0;
+	for(i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+			if(b[i]==b[j]){
+				b[j]=0;
+			}
+		}
+	}
+	for(i=0;i<n;i++){
+		if(b[i]!=0)count++;	
+	}
+	printf("%d\n",count);
+	for(i=0;i<n;i++){
+		if(b[i]!=0)
+			printf("%d ",b[i]);
+	}
+	printf("\b");
+	return 0;
+}
+	
+int qiuhe(int x){
+	int a[4]={0};
+	int z,i;
+	z=0;
+	for(i=0;i<4;i++){
+		a[i]=x%10;
+		x=x/10;
+		z=z+a[i];
+	}
+	return z;
+}
