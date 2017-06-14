c040c551537fce7e65f631777152281b2ea55cb7
fengzi2016 <1820166258@qq.com>
2017-06-14 22:36:21 +0800

    PAT(1008)

--- PAT/PAT(1008)[有错].c
+++ PAT/PAT(1008)[有错].c
@@ -11,7 +11,31 @@ int main(){
 		a[j--]=a[i];
 	}
 	for(i=0;i<n;i++){
-		printf("%d%c",a[i],(n-1-i)!=0?' ' : '\0');
+		printf("%d%c",a[i],(n-1-i)!=0?' ': '\0');
 		//printf("%d ",a[i]);
 	}
-}                                
+}  
+/*#include<stdio.h> 
+int main(){
+	int i, N,M;
+	int a[105];
+	scanf("%d %d",&N,&M);
+	M=M%N;
+	for(i=0;i<N;i++){
+		scanf("%d",a+i);
+	}
+	int count=0;//用来计数，为了“最后一个元素不输出空格服务”
+	for( i=N-M; i<N;i++){
+		count++;
+		printf("%d",a[i]); 
+		if(count<N)
+			printf(" ");
+	}
+	for(i=0;i<N-M;i++){
+		count++;
+		printf("%d",a[i]);
+		if(count<N)printf(" ");
+	}
+	return 0;
+}
+*/                              
---
+++ PAT/可以动态输入数组元素.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a[1000];
+	do{
+		scanf("%d",&a[i]);
+		i++;
+	}while((ch=getchar())!='\n');
+	
+	
+	
+	
+	
+	
+}
