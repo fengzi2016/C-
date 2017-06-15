6897481dde0b034f6e16f900558064359f74f6e8
fengzi2016 <1820166258@qq.com>
2017-06-15 19:27:15 +0800

    PAT（1010）

--- PAT/可以动态输入数组元素.c
+++ PAT/可以动态输入数组元素.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 int main(){
-	int a[1000];
+	int i;
+	char ch;
+	int a[10];
+	i=0;
 	do{
 		scanf("%d",&a[i]);
 		i++;
 	}while((ch=getchar())!='\n');
-	
-	
+	for(i=0;i<10;i++)
+	printf("%d",a[i]);
 	
 	
 	
