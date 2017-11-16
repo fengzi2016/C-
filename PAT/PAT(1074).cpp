700f04dbe5e4254a66ea13560b623c8990a0345f
fengzi2016 <1820166258@qq.com>
2017-11-16 23:44:13 +0800

    others1074

--- PAT/PAT(1074).cpp
+++ PAT/PAT(1074).cpp
@@ -23,3 +23,44 @@ int main(){
 	}
 	
 }
+
+// #include <bits/stdc++.h>
+
+// using namespace std;
+// void Inverted(char *a,int *numa)//转化函数
+// {
+//     for(int i=strlen(a)-1,j=0; i>=0; i--,j++)
+//         numa[j]=a[i]-'0';
+// }
+
+// int main()
+// {
+//     int temp,numa[25]= {0},numb[25]= {0},numc[25]= {0},flag=0,i;
+//     char a[25],b[25],c[25];
+//     cin>>a>>b>>c;
+//     Inverted(a,numa);
+//     Inverted(b,numb);
+//     Inverted(c,numc);
+
+//     for(int i=0; i<25; i++)//numa[]用来存储进制，控制0代表10进制
+//         if(numa[i]==0)
+//             numa[i]=10;
+
+//     for(int i=0; i<25; i++)//加法实现，用numb[]来存储要输出的结果
+//     {
+//         temp=numb[i]+numc[i]+flag;
+//         numb[i]=temp%numa[i];
+//         flag=temp/numa[i];
+//     }
+
+//     for(i=24;i>=0; i--)//控制结果输出
+//     {
+//         if(numb[i]!=0)break;
+//         if(numb[i]==0)numb[i]=-1;
+//     }
+
+//     if(i==-1)printf("0");//如果结果全部为0输出0.
+//     for(; i>=0; i--)//输出答案
+//         printf("%d",numb[i]);
+//     return 0;
+// }
