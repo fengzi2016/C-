2d59dd47d1c7afac1817a0a264fdd9ff3edcf2e7
fengzi2016 <1820166258@qq.com>
2018-03-07 21:15:00 +0800

    not ok

--- PAT/PAT(1078)(not end).cpp
+++ PAT/PAT(1078)(not end).cpp
@@ -4,46 +4,50 @@
 #include <string.h>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 using namespace std; 
 int main(){
-	string a;
+	char a[1000];
 	string compresscode(string a);
 	string decode(string a);
     char order;
     cin>>order;
-    cin>>a;
+    gets(a);
     if(order=='C'){
-    	
+    	decode(a);
     }else if(order == 'D'){
-    	cout<<decode(a);
+    	
+    	
     	
     }
 }
-string decode(string a){
+string decode(char a[]){
+	
+	
 	int i,j;
 	string fina = "";
 	int count=1,black_count = 1;
-	for(i=0;i<a.length();i++){
-		for(j=i+1;j<a.length();j++){
+	for(i=0;a[i]!='/0';i++){
+		for(j=i+1;a[j]!='/0';j++){
 			if(a[i]==a[j]&&a[i]!=' '){
 				count++;
 			}else if(a[i]==a[j]&&a[i]==' '){
 				black_count++;
 			}
 			if(a[i]!=a[j]&&count!=1){
-				count = count + '0'+a[i];
-				fina + = count;
+		        cout<<count<<a[i];
 				break;
 			}
 		 	if(a[i]!=a[j]&&black_count!=1){
-				 fina + =a[i];
+		 		cout<<a[i];
 				 break;
 			}
-		
+			cout<<i;
 		}
 	i = j;
 	j = i+1;
-	
+	count=1;
+	black_count=1;
 	}
 	return fina;
 	
