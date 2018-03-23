210a3e7319f460c91334191a8d3b016f571cdaae
fengzi2016 <1820166258@qq.com>
2018-03-23 21:29:06 +0800

    add 1002

---
+++ PAT/PATJ(1002).cpp
@@ -0,0 +1,76 @@
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctype.h>
+#include <math.h>
+using namespace std; 
+int main(){
+	
+	int len1,len2,i,j;
+	cin>>len1;
+	len1 = len1 * 2 ;
+	float arr1[len1] ;
+	for(i=0;i<len1;i++){
+		cin>>arr1[i];
+	}
+	cin>>len2;
+	len2 = len2 * 2;
+	float arr2[len2];
+	for(i=0;i<len2;i++){
+		cin>>arr2[i];
+	}
+	float zhishuArr[11],xishuArr[11];
+	int count = 0;
+	i = 0,j=0;
+	int k = 0,t=0;
+	while(i!=len1&&j!=len2){
+		if(arr1[i]>arr2[j]){
+			zhishuArr[k++]=arr1[i+1];
+			xishuArr[t++] = arr1[i];
+			i = i+2;
+			count ++ ;
+			
+		}else if(arr1[i]==arr2[j]){
+			zhishuArr[k++] =  arr1[i+1]+arr2[j+1];
+			xishuArr[t++] =  arr1[i];
+		    i = i+2;
+		    j = j+2;
+		    count ++ ;
+		    
+		}else{
+			zhishuArr[k++] =  arr2[j+1];
+			xishuArr[t++] =  arr2[j];
+		    j = j+2;
+		    count ++;
+			
+		}
+	
+	}
+	if(len1!=i) {
+		zhishuArr[k++] =  arr1[i+1];
+		xishuArr[t++] =  arr1[i];
+		i = i + 2;
+		count ++;
+	}
+	if(len2!=j){
+		zhishuArr[k++] =  arr2[j+1];
+		xishuArr[t++] =  arr2[j];
+		j = j+2;
+		count ++;
+	}
+	if(count != 0)
+		cout<<count<<" ";
+	for(i=0;i<count;i++){
+		if(zhishuArr != 0){
+			
+			cout<<xishuArr[i]<<" "<<zhishuArr[i];
+			if(i!=count-1) cout<<" ";
+		}
+		
+		
+		
+	}
+	
+}
+ 
--- PAT/PATJ（1001）.c
+++ PAT/PATJ（1001）.c
@@ -1,57 +1,23 @@
-//思路 相加之后再除以1000的倍数，根据a,b的范围可以知道sum最大为2000000，最小为-2000000，即1000*1000*1000之间
-#include <stdio.h> 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctype.h>
+using namespace std; 
 int main(){
-	long int a,b;
-	scanf("%ld %ld",&a,&b);
-	long int sum,reserv;
-	sum = a+b;
-	reserv = sum;
-	int i,chushu = 1000000;
-	int temp;
-	int array[3];
-	int weishu(int n);
-	for(i=0;i<3;i++){
-		temp = sum/chushu;
-		array[i]=temp;
-		sum = sum %chushu;
-		chushu /=1000;
-	}
-	for(i=0;i<3;i++){
-	  if(array[i]<0){
-			array[i] *= -1;
+	int a,b,sum;
+	while(cin>>a>>b){
+		sum = a+b;
+		if(sum<0){
+			printf("-");
+			sum = - sum;
 		}
-	}
-	
-	if(reserv<0){
-	   printf("-");
-	}
-	if(array[0]==0&&array[1]==0){
-		printf("%d",array[2]);
-	}else if(array[0]==0){
-		printf("%d,",array[1]);
-		for(i=0;i<3-weishu(array[2]);i++)
-		 printf("0");
-		printf("%d",array[2]);
-	}else{
-	    printf("%d,",array[0]);
-	    for(i=0;i<3-weishu(array[1]);i++)
-		 printf("0");
-		printf("%d,",array[1]);
-		for(i=0;i<3-weishu(array[2]);i++)
-		 printf("0");
-		printf("%d",array[2]);
-		
-	}
+		if(sum>=1000000){
+			printf("%d,%03d,%03d\n",sum/1000000,sum/1000%1000,sum%1000);
+		}else if(sum>=1000){
+			printf("%d,%03d\n",sum/1000,sum%1000);
+		}else{
+			printf("%d\n",sum);
+		}
+	} 
 	return 0;
-}
-int weishu(int n){
-    int count = 0;
- 
-    while(n != 0)
-    {
-        n /= 10;
-        ++count;
-    }
-    return count;
- 
-}
+} 
