efff19261929fed10c6a4a7d846c0f6c82fb199f
fengzi2016 <1820166258@qq.com>
2018-03-25 22:26:11 +0800

    add some questions

---
+++ PAT/PATJ(1003)(not ending).c
@@ -0,0 +1,7 @@
+# include<iostream>
+# include <algorithm>
+ using namespace std;
+ int cityNum ,roadNum, myCity,needCity;
+ cin>>cityNum>>roadNum>>myCity>>needCity;
+ int map[500][500],weight[500][500],dis[500],num[500];
+ 
