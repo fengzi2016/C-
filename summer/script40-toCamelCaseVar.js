9d30ed5e37f084e7234c82bcb4b1285ee1d4bc01
fengzi2016 <1820166258@qq.com>
2018-08-24 12:06:18 +0800

    feat: add toCamelCaseVar

---
+++ summer/script40-toCamelCaseVar.js
@@ -0,0 +1,27 @@
+const toCamelCaseVar = (variable) => {
+    variable = variable.trim();
+    let arr=variable.split('');
+    let start = 0;
+    let end = arr.length-1;
+    let countEnd = 0;
+    while(arr[start]==='_'){start++};
+    while(arr[end]==='_'){end--;countEnd++}
+    arr = [...arr.slice(start,end+1)];
+    for(let i = 0; i<arr.length; i++) {
+        if(arr[i]==='_') {
+          let k = 0;
+          while(arr[i+k+1]==='_') {k++;}
+          arr = [...arr.slice(0,i),arr[i+1+k].toUpperCase(),...arr.slice(i+2+k,end+1)];
+        }
+        
+    }
+    let result =  arr.join('');
+    result=result.padStart(result.length+start,'_');
+    result=result.padEnd(countEnd+result.length,'_');
+    return result;
+  
+}
+
+console.log(toCamelCaseVar("  __thisIs_myName_ "))
+console.log(toCamelCaseVar("this_is_my_name"))
+console.log(toCamelCaseVar('_$this_is_my_name'))
