8010b7fb0f0215515e663b839202b0dd002601ad
fengzi2016 <1820166258@qq.com>
2018-09-05 21:35:19 +0800

    feat: add more

--- summer/leetcode34-conJunmp.cpp
+++ summer/leetcode34-conJunmp.cpp
@@ -29,3 +29,15 @@ int main() {
         else return false;
         
     }
+
+    bool canJump(vector<int>& nums)  {
+        int  n = nums.size(), i = 0,cur = 0;
+        while(cur<n-1) {
+            int pre = cur;
+            for(;i<=pre;i++) {
+                cur = max(cur,i+nums[i]);
+            }
+            if(pre==cur)return -1;
+        }
+        if()
+    }
